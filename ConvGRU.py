5cf99e5d6bda5580f521364de32a0732cd447647
MinkyuChoi <minkyu.choi8904@gmail.com>
2018-11-09 15:06:30 -0500

    Updated codes to support CPU. Added test code.

--- ConvGRU.py
+++ ConvGRU.py
@@ -1,16 +1,14 @@
-// Tensor and Variable in pytorch are different. Variable is wrapper of tensor. Tensor is just 'data' storing values. And variable is including tensor and having more things like relationships between variables and gradients. Therefore, when calculating gradient to do BP, we need to use variable, not tensor. But when we need just tensor from variable, using .data would output tensor. 
-// a = b.data
-
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
-class ConvGPUCell(nn.Module):
-    def __init__(self, input_shape, hidden_c, kernel_shape):
+class ConvGRUCell(nn.Module):
+    def __init__(self, input_shape, hidden_c, kernel_shape, isGPU):
         """
         input_shape: (channel, h, w)
         hidden_c: the number of hidden channel.
         kernel_shape: (h, w)
+        isGPU: True/False
         """
         super().__init__()
         self.input_c, self.input_h, self.input_w = input_shape
@@ -18,8 +16,8 @@ class ConvGPUCell(nn.Module):
         self.kernel_h, self.kernel_w = kernel_shape
         self.padding_same_h = self.kernel_h // 2
         self.padding_same_w = self.kernel_w // 2
+        self.isGPU = isGPU
         
-        #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True) 
         self.gate_conv = nn.Conv2d(in_channels=self.input_c + self.hidden_c,
                 out_channels=self.hidden_c * 2, 
                 kernel_size=(self.kernel_h, self.kernel_w), 
@@ -47,6 +45,9 @@ class ConvGPUCell(nn.Module):
         return state_cur
 
     def set_init_hidden_state(self, batch_size):
-        self.init_hidden = (Variable(torch.zeros(batch_size, self.hidden_c, self.hidden_h, self.hidden_w)).cuda(), 
-            Variable(torch.zeros(batch_size, self.hidden_c, self.hidden_h, self.hidden_w)).cuda())
+        if self.isGPU:
+            self.init_hidden = Variable(torch.zeros(batch_size, self.hidden_c, self.input_h, self.input_w)).cuda()
+        else:
+            self.init_hidden = Variable(torch.zeros(batch_size, self.hidden_c, self.input_h, self.input_w))
+            
         return self.init_hidden
--- ConvLSTM.py
+++ ConvLSTM.py
@@ -1,16 +1,14 @@
-// Tensor and Variable in pytorch are different. Variable is wrapper of tensor. Tensor is just 'data' storing values. And variable is including tensor and having more things like relationships between variables and gradients. Therefore, when calculating gradient to do BP, we need to use variable, not tensor. But when we need just tensor from variable, using .data would output tensor. 
-// a = b.data
-
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
 class ConvLSTMCell(nn.Module):
-    def __init__(self, input_shape, hidden_c, kernel_shape):
+    def __init__(self, input_shape, hidden_c, kernel_shape, isGPU):
         """
         input_shape: (channel, h, w)
         hidden_c: the number of hidden channel.
         kernel_shape: (h, w)
+        isGPU: True/False
         """
         super().__init__()
         self.input_c, self.input_h, self.input_w = input_shape
@@ -18,13 +16,21 @@ class ConvLSTMCell(nn.Module):
         self.kernel_h, self.kernel_w = kernel_shape
         self.padding_same_h = self.kernel_h // 2
         self.padding_same_w = self.kernel_w // 2
+        self.isGPU = isGPU
         
-        #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True) 
         self.gate_conv = nn.Conv2d(in_channels=self.input_c + self.hidden_c,
                 out_channels=self.hidden_c * 4, 
                 kernel_size=(self.kernel_h, self.kernel_w), 
                 stride=1, 
                 padding=(self.padding_same_h, self.padding_same_w))
+
+
+
+        self.cal =  nn.Conv2d(in_channels=hidden_c, 
+                out_channels=input_shape[0], 
+                kernel_size=(3,3), 
+                stride=1,
+                padding=(3//2, 3//2))
 
     def forward(self, input_cur, state_prev):
         hidden_prev, cell_prev = state_prev
@@ -43,6 +49,9 @@ class ConvLSTMCell(nn.Module):
         return hidden_cur, cell_cur
 
     def set_init_hidden_state(self, batch_size):
-        self.init_hidden = (Variable(torch.zeros(batch_size, self.hidden_c, self.hidden_h, self.hidden_w)), 
-            Variable(torch.zeros(batch_size, self.hidden_c, self.hidden_h, self.hidden_w)))
+        if self.isGPU:
+            self.init_hidden = (Variable(torch.zeros(batch_size, self.hidden_c, self.input_h, self.input_w).cuda()), Variable(torch.zeros(batch_size, self.hidden_c, self.input_h, self.input_w)).cuda())
+        else:
+            self.init_hidden = (Variable(torch.zeros(batch_size, self.hidden_c, self.input_h, self.input_w)), Variable(torch.zeros(batch_size, self.hidden_c, self.input_h, self.input_w)))
         return self.init_hidden
+
